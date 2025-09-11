faab19f74c54b63d9b1fc0230c1d5312231ef992
guelfi <guelfi@msn.com>
2025-09-11 10:28:30 -0300

    refactor(web): Remove custom entrypoint e USER nginx do Dockerfile

--- src/Web/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Create and set permissions for the Nginx PID directory at runtime
-mkdir -p /run/nginx
-chown -R nginx:nginx /run/nginx
-
-# Execute the original Nginx command
-exec nginx -g 'daemon off;'
